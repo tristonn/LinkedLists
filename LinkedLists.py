21bb351982915bc9cc02462e51930546592031fb
Triston Lehmann <tristonlehmann@gmail.com>
2025-03-26 20:34:51 -0700

    Added some more functionality to the SinglyLinkedList class

--- LinkedLists.py
+++ LinkedLists.py
@@ -1,16 +1,49 @@
 class Node:
-    def __init__(self, value, link_node = None):
+    def __init__(self, value, next_node = None):
         self.value = value
-        self.link_node = link_node
+        self.next_node = next_node
 
     def get_value(self):
         return self.value
     
-    def set_value(self, value):
-        self.value = value
+    def get_next_node(self):
+        return self.next_node
 
-    def set_link_node(self, link_node):
-        self.link_node = link_node
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+class SinglyLinkedList:
+    def __init__(self, value):
+        self.head_node = Node(value)
+
+    def get_head_node(self):
+        return self.head_node
+    
+    def insert_beginning(self, value):
+        new_node = Node(value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_node
+
+    def stringify_the_list(self):
+        the_list = ""
+        current_node = self.head_node
+        while current_node:
+            if current_node.get_value() is not None:
+                if current_node.get_next_node() == None:
+                    the_list += str(current_node.get_value())
+                else:
+                    the_list += str(current_node.get_value()) + "->"
+            current_node = current_node.get_next_node()
+        return the_list
+
+#testing out functionality
+my_linked_list = SinglyLinkedList(25)
+my_linked_list.insert_beginning(24)
+my_linked_list.insert_beginning(55)
+my_linked_list.insert_beginning(52)
+my_linked_list.insert_beginning(22)
+my_linked_list.insert_beginning(21)
+print(my_linked_list.stringify_the_list())
 
 
 
