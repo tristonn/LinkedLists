5eca64dbe99041770f13fe8fd3d4f78523fb9288
Triston Lehmann <tristonlehmann@gmail.com>
2025-03-28 18:20:15 -0700

    Adding some final touches and cleaning up code

--- LinkedLists.py
+++ LinkedLists.py
@@ -27,13 +27,15 @@ class SinglyLinkedList:
     def stringify_the_list(self):
         the_list = ""
         current_node = self.head_node
+
         while current_node:
             if current_node.get_value() is not None:
-                if current_node.get_next_node() == None:
+                if current_node.get_next_node() is None:
                     the_list += str(current_node.get_value())
                 else:
-                    the_list += str(current_node.get_value()) + "->"
+                    the_list += str(current_node.get_value()) + "->"    
             current_node = current_node.get_next_node()
+
         return the_list
     
     def remove_node(self, value):
@@ -74,31 +76,31 @@ class DoublyLinkedList:
     def __init__(self):
         self.head_node = None
         self.tail_node = None
+  
+    def add_to_head(self, new_value):
+        new_head = BiDirNode(new_value)
+        current_head = self.head_node
 
-    def add_to_head(self, value):
-        new_head_node = BiDirNode(value)
-        current_head_node = self.head_node
+        if current_head != None:
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
 
-        if current_head_node is not None:
-            current_head_node.set_prev_node(new_head_node)
-            new_head_node.set_next_node(current_head_node)
+        self.head_node = new_head
 
-        self.head_node = new_head_node
+        if self.tail_node == None:
+            self.tail_node = new_head
 
-        if self.tail_node is None:
-            self.tail_node = new_head_node
-
-    def add_to_tail(self, value):
-        new_tail = BiDirNode(value)
+    def add_to_tail(self, new_value):
+        new_tail = BiDirNode(new_value)
         current_tail = self.tail_node
 
-        if current_tail is not None:
+        if current_tail != None:
             current_tail.set_next_node(new_tail)
             new_tail.set_prev_node(current_tail)
-        
+
         self.tail_node = new_tail
 
-        if self.head_node is None:
+        if self.head_node == None:
             self.head_node = new_tail
 
     def remove_head(self):
@@ -133,5 +135,43 @@ class DoublyLinkedList:
 
         return removed_tail.get_value()
 
+    def remove_by_value(self, value_to_remove):
+        node_to_remove = None
+        current_node = self.head_node
+
+        while current_node != None:
+            if current_node.get_value() == value_to_remove:
+                node_to_remove = current_node
+                break
+
+        current_node = current_node.get_next_node()
+
+        if node_to_remove == None:
+            return None
+
+        if node_to_remove == self.head_node:
+            self.remove_head()
+            
+        elif node_to_remove == self.tail_node:
+            self.remove_tail()
+
+        else:
+            next_node = node_to_remove.get_next_node()
+            prev_node = node_to_remove.get_prev_node()
+            next_node.set_prev_node(prev_node)
+            prev_node.set_next_node(next_node)
+
+        return node_to_remove
+
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.head_node
+
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+
+        return string_list
 
     
